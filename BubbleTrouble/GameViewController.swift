ded0334462610313277f27fc5dfa797ebe9abbca
Hana Harris <Selkiefawn@gmail.com>
2025-03-24 15:35:07 -0400

    Initial Commit

---
+++ BubbleTrouble/GameViewController.swift
@@ -0,0 +1,34 @@
+//
+//  GameViewController.swift
+//  BubbleTrouble
+//
+//  Created by Morgan Harris on 3/24/25.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+}
